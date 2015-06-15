abe6112da80942307fc9a3a941872a32b77d6d9f
Alorel <a.molcanovas@gmail.com>
2015-06-15 21:47:22 +0100

    table creations

---
+++ setup/MySQL/create_locale_table.sql
@@ -0,0 +1,18 @@
+-- Since this table will not hold very large amounts of data, the language code column is not a numeric ID pointing to
+-- a different table
+
+CREATE TABLE `alo_locale` (
+	`id`    INT UNSIGNED                 NOT NULL AUTO_INCREMENT,
+	`lang`  CHAR(2)
+	        COLLATE `ascii_general_ci`   NOT NULL,
+	`page`  VARCHAR(25)
+	        COLLATE `ascii_general_ci`   NOT NULL,
+	`key`   VARCHAR(25)
+	        COLLATE `ascii_general_ci`   NOT NULL,
+	`value` TEXT
+	        COLLATE `utf8mb4_general_ci` NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY (`lang`, `page`, `key`)
+)
+	ENGINE = InnoDB
+	DEFAULT CHARSET = `utf8mb4`;
