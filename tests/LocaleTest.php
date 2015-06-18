a2cfcf8581f3d4ef5d3aebb4386bbae0558f26b8
Alorel <a.molcanovas@gmail.com>
2015-06-18 23:59:06 +0100

    localetest fix for travis

--- tests/LocaleTest.php
+++ tests/LocaleTest.php
@@ -1,25 +1,6 @@
 <?php
 
-   use Alo\Db\MySQL;
-   use Alo\Locale;
-
    class LocaleTest extends PHPUnit_Framework_TestCase {
-
-      /**
-       * @var Locale
-       */
-      private $locale;
-
-      /**
-       * @var MySQL
-       */
-      private $db;
-
-      function __construct($name = null, array $data = [], $dataName = '') {
-         parent::__construct($name, $data, $dataName);
-         $this->db     = new MySQL();
-         $this->locale = new Locale($this->db);
-      }
 
       function testTmp() {
          $this->assertTrue(true);
