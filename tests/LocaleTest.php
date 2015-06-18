29c5fa9538fe1281598298c5cbb488a4e3d2a7d1
Alorel <a.molcanovas@gmail.com>
2015-06-18 20:38:54 +0100

    trying to exlude composer.json from sensiolabs

---
+++ tests/LocaleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class LocaleTest extends PHPUnit_Framework_TestCase {
+
+   /**
+    * @var Locale
+    */
+   private $locale;
+
+   function __construct() {
+      parent::__construct();
+   }
+}
