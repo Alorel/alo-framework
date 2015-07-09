de9a328df260b2c9ea7a57c3dc62959eb9fb23c4
Alorel <a.molcanovas@gmail.com>
2015-07-09 19:39:34 +0100

    constants shrunk in index.php, many moved to sys/core/global_defines.php

--- src/index.php
+++ src/index.php
@@ -1,130 +1,28 @@
 <?php
 
-    /**
-     * To calculate generation time
-     *
-     * @var float
-     */
+    /** Microtime float when the script started */
     define('GEN_START', microtime(true));
 
-    // ===== Miscellaneous constants' definitions BEGIN =====
+    /** root/index.php directory */
+    define('DIR_INDEX', __DIR__ . DIRECTORY_SEPARATOR);
 
-    /**
-     * Defines a logging level as "debug"
-     *
-     * @var int
-     */
-    define('LOG_LEVEL_DEBUG', 3);
-
-    /**
-     * Defines a logging level as "warning"
-     *
-     * @var int
-     */
-    define('LOG_LEVEL_WARNING', 2);
-
-    /**
-     * Defines a logging level as "error"
-     *
-     * @var int
-     */
-    define('LOG_LEVEL_ERROR', 1);
-
-    /**
-     * Defines a logging level as "no logging"
-     *
-     * @var int
-     */
-    define('LOG_LEVEL_NONE', 0);
-
-    /**
-     * Defines the environment as "setup"
-     *
-     * @var int
-     */
-    define('ENV_SETUP', 0);
-
-    /**
-     * Defines the environment as "development"
-     *
-     * @var int
-     */
-    define('ENV_DEVELOPMENT', 1);
-
-    /**
-     * Defines the environment as "production"
-     *
-     * @var int
-     */
-    define('ENV_PRODUCTION', 2);
-
-    // ===== Miscellaneous constants' definitions END =====
+    require_once 'sys' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'global_defines.php';
 
     // ===== General setup BEGIN =====
 
-    /**
-     * Defines the log level. Valid values are LOG_LEVEL_DEBUG, LOG_LEVEL_ERROR and LOG_LEVEL_NONE
-     *
-     * @var string
-     */
+    /** The application environment. Valid values are "setup" "dev" */
+    define('ENVIRONMENT', ENV_SETUP);
+
+    /** Defines the log level. Valid values are LOG_LEVEL_NONE LOG_LEVEL_DEBUG, LOG_LEVEL_WARNING and LOG_LEVEL_ERROR */
     define('LOG_LEVEL', LOG_LEVEL_WARNING);
 
     /**
-     * If logging is set to intense, an entire debug backtrace will be appended to
-     * each row, otherwise it's just the last file and line number
-     *
-     * @var bool
+     * If logging is set to intense, an entire debug backtrace will be appended to each row, otherwise it's just the
+     * last file and line number
      */
     define('LOG_INTENSE', false);
 
-    /**
-     * The application environment. Valid values are "setup" "dev
-     */
-    define('ENVIRONMENT', ENV_SETUP);
-
     // ===== General setup END. You needn't change anything below this line =====
-
-    /**
-     * Index directory
-     *
-     * @var string
-     */
-    define('DIR_INDEX', __DIR__ . DIRECTORY_SEPARATOR);
-
-    /**
-     * Application directory
-     *
-     * @var string
-     */
-    define('DIR_APP', DIR_INDEX . 'app' . DIRECTORY_SEPARATOR);
-
-    /**
-     * The temp directory
-     *
-     * @var string
-     */
-    define('DIR_TMP', DIR_APP . 'tmp' . DIRECTORY_SEPARATOR);
-
-    /**
-     * System directory
-     *
-     * @var string
-     */
-    define('DIR_SYS', DIR_INDEX . 'sys' . DIRECTORY_SEPARATOR);
-
-    /**
-     * Controllers directory
-     *
-     * @var string
-     */
-    define('DIR_CONTROLLERS', DIR_APP . 'controllers' . DIRECTORY_SEPARATOR);
-
-    /**
-     * The minimum available PHP integer
-     *
-     * @var int
-     */
-    define('PHP_INT_MIN', ~PHP_INT_MAX);
 
     // Change dir to make sure CLI requests are correct
     if (defined('STDIN')) {
---
+++ src/sys/core/global_defines.php
@@ -0,0 +1,37 @@
+<?php
+
+    /** Defines a logging level as "debug" */
+    define('LOG_LEVEL_DEBUG', 3);
+
+    /** Defines a logging level as "warning" */
+    define('LOG_LEVEL_WARNING', 2);
+
+    /** Defines a logging level as "error" */
+    define('LOG_LEVEL_ERROR', 1);
+
+    /** Defines a logging level as "no logging" */
+    define('LOG_LEVEL_NONE', 0);
+
+    /** Defines the environment as "setup" */
+    define('ENV_SETUP', 0);
+
+    /** Defines the environment as "development" */
+    define('ENV_DEVELOPMENT', 1);
+
+    /** Defines the environment as "production" */
+    define('ENV_PRODUCTION', 2);
+
+    /** Application directory */
+    define('DIR_APP', DIR_INDEX . 'app' . DIRECTORY_SEPARATOR);
+
+    /** The temp directory */
+    define('DIR_TMP', DIR_APP . 'tmp' . DIRECTORY_SEPARATOR);
+
+    /** System directory */
+    define('DIR_SYS', DIR_INDEX . 'sys' . DIRECTORY_SEPARATOR);
+
+    /** Controllers directory */
+    define('DIR_CONTROLLERS', DIR_APP . 'controllers' . DIRECTORY_SEPARATOR);
+
+    /** The minimum available PHP integer */
+    define('PHP_INT_MIN', ~PHP_INT_MAX);
